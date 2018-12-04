d3e1821df73d43c0b12d9b9a2d4af8ea71ca8df8
ErikNPeterson <erikeh1@gmail.com>
2018-12-04 17:54:22 -0500

    completed the first 5 exercises

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,5 +8,32 @@ puts "----------"
 
 # Your code goes here ...
 
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true , womens_apparel: true)
 
-# store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true , womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false , womens_apparel: true)
+
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true , womens_apparel: false)
+
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false , womens_apparel: true)
+
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true , womens_apparel: false)
+
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true , womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts @mens_stores
+
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name} . Annual Revenue #{store.annual_revenue} ."
+  end
+
+  @womens_stores_under1M = Store.where(womens_apparel: true, annual_revenue: 1..999999)
+
+  @womens_stores_under1M.each do |store|
+    puts "Store Name: #{store.name} . Annual Revenue #{store.annual_revenue} ."
+    end
+
+# Client.where("orders_count = ?", params[:orders])
+
+
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,20 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+
+
 # Your code goes here ...
+@total_stores = Store.count
+
+puts "The total # of company stores is #{@total_stores}"
+
+@company_revenue = Store.sum("annual_revenue")
+puts "The company revenue is $ #{@company_revenue}" 
+puts "The average store revenue is $ #{@company_revenue/@total_stores}" 
+
+@stores_over_1m = Store.where( annual_revenue: 1000000..Float::INFINITY).count
+
+# .where("annual_revenue >= ?", 1000000) # or 
+# .where("annual_revenue >= 1000000")
+
+puts "The company has #{@stores_over_1m} stores that are generating $1M or more in annual sales."
